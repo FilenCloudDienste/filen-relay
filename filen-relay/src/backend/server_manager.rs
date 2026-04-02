80f6b08cd80998145d0c33565bb7b8c4639beb12
JupiterPi <hello@jupiterpi.de>
2026-04-02 12:28:50 +0200

    refactor: move rclone request handler to server_manager, fix some details along the way

--- filen-relay/src/backend/server_manager.rs
+++ filen-relay/src/backend/server_manager.rs
@@ -9,20 +9,32 @@ use strum::IntoEnumIterator as _;
 use tokio::io::AsyncBufReadExt;
 use tokio::io::BufReader;
 
+use crate::backend::db::DB;
 use crate::backend::rclone_auth_proxy::generate_rclone_auth_proxy_args;
 use crate::common::ServerType;
+
+use std::str::FromStr;
+
+use axum_core::body::Body;
+use axum_core::response::IntoResponse;
+use base64::prelude::BASE64_STANDARD;
+use base64::Engine;
+use dioxus::server::axum;
+use dioxus::server::axum::{extract::Request, response::Response};
+use http::Uri;
 
 pub(crate) struct ServerManager {
     processes: Vec<RcloneServerProcess>,
 }
 
 struct RcloneServerProcess {
-    pub server_type: ServerType,
-    pub _process: tokio::process::Child,
-    pub port: u16,
+    server_type: ServerType,
+    _process: tokio::process::Child,
+    port: u16,
+    base_url: Option<&'static str>,
 }
 
-pub(crate) static RCLONE_BASE_URL_TO_SUBSTITUTE: &str = "/RCLONE_BASE_URL_TO_SUBSTITUTE"; // must start with a slash
+static RCLONE_BASE_URL_TO_SUBSTITUTE: &str = "/RCLONE_BASE_URL_TO_SUBSTITUTE"; // must start with a slash
 
 impl ServerManager {
     pub(crate) async fn start_servers(self_port: u16) -> Result<ServerManager> {
@@ -31,6 +43,11 @@ impl ServerManager {
             .join("rclone_configs");
         let mut servers = vec![];
         for server_type in ServerType::iter() {
+            if server_type == ServerType::Http {
+                // we don't need a separate http server, since the WebDAV server already handles this
+                continue;
+            }
+
             // start server process
             let port =
                 port_check::free_local_ipv4_port().context("Failed to find free local port")?;
@@ -55,6 +72,22 @@ impl ServerManager {
             // todo: can we avoid creating a platform-dependent shell script?
 
             // spawn rclone process
+            let port_str = format!(":{}", port);
+            let mut args = vec![
+                "serve",
+                server_type.to_str(),
+                "--addr",
+                &port_str,
+                "--auth-proxy",
+                script_file.path().to_str().unwrap(),
+            ];
+            let base_url = if server_type == ServerType::Webdav {
+                args.push("--baseurl");
+                args.push(RCLONE_BASE_URL_TO_SUBSTITUTE);
+                Some(RCLONE_BASE_URL_TO_SUBSTITUTE)
+            } else {
+                None
+            };
             let (mut process, _) =
                 RcloneInstallation::initialize_unauthenticated(&RcloneInstallationConfig {
                     rclone_binary_dir: config_dir.clone(),
@@ -62,16 +95,7 @@ impl ServerManager {
                 })
                 .await
                 .context("Failed to initialize Rclone installation")?
-                .execute_in_background(&[
-                    "serve",
-                    server_type.to_str(),
-                    "--addr",
-                    &format!(":{}", port),
-                    "--auth-proxy",
-                    script_file.path().to_str().unwrap(),
-                    "--baseurl",
-                    RCLONE_BASE_URL_TO_SUBSTITUTE,
-                ])
+                .execute_in_background(&args)
                 .await
                 .context("Failed to start Rclone server")?;
 
@@ -103,21 +127,185 @@ impl ServerManager {
                 server_type,
                 _process: process,
                 port,
+                base_url,
             });
         }
 
         Ok(ServerManager { processes: servers })
     }
 
-    pub(crate) async fn get_port_for_forwarded_request(
+    pub(crate) async fn handle_rclone_request(
         &self,
+        self_port: &u16,
         server_type: &ServerType,
-    ) -> Result<u16> {
-        let server = self
-            .processes
-            .iter()
-            .find(|s| s.server_type == *server_type)
-            .context("No server found for the given type")?;
-        Ok(server.port)
+        id: String,
+        mut req: Request,
+    ) -> Result<Response, anyhow::Error> {
+        let shares = DB
+            .get_shares()
+            .map_err(|e| anyhow::anyhow!("Failed to get shares from database: {}", e))?;
+        if let Some(share) = shares.into_iter().find(|s| s.id.short() == id) {
+            // check password
+            if let Some(password) = &share.password {
+                let auth_header = req
+                    .headers()
+                    .get("authorization")
+                    .and_then(|h| h.to_str().ok())
+                    .unwrap_or("");
+                let auth_header = auth_header.strip_prefix("Basic ").unwrap_or("");
+                let auth_header = BASE64_STANDARD.decode(auth_header).unwrap_or_default();
+                let auth_header = String::from_utf8_lossy(&auth_header);
+                if !auth_header.ends_with(password) {
+                    return Ok(Response::builder()
+                        .status(axum::http::StatusCode::UNAUTHORIZED)
+                        .header(
+                            "WWW-Authenticate",
+                            format!("Basic realm=\"share {}\"", share.id),
+                        )
+                        .body(Body::from(
+                            "This share needs a password to access. No username is needed.",
+                        ))
+                        .unwrap());
+                }
+            }
+
+            // insert share id for auth proxy
+            req.headers_mut().insert(
+                "Authorization",
+                format!("Basic {}", BASE64_STANDARD.encode(format!("{}:", share.id)))
+                    .parse()
+                    .unwrap(),
+            );
+
+            // find proxy target
+            let server = self
+                .processes
+                .iter()
+                .find(|s| {
+                    s.server_type == *server_type
+                        || (*server_type == ServerType::Http && s.server_type == ServerType::Webdav)
+                    // see above about not needing a separate HTTP server
+                })
+                .context("No server found for the given type")?;
+            let proxy = axum_reverse_proxy::ReverseProxy::new(
+                "",
+                &format!("http://127.0.0.1:{}", server.port),
+            );
+
+            // transform urls (request uri and several headers)
+            // this converts between e.g. /webdav/fc127fba/directory/file.txt -> /RCLONE_BASE_URL_TO_SUBSTITUTE/directory/file.txt
+            // and back in XML responses from WebDAV
+            let (transform_uri_fordward, transform_uri_backward) = {
+                let base_path = {
+                    let request_path = req.uri().to_string();
+                    request_path
+                        .find(&id)
+                        .map(|id_idx| &request_path[..(id_idx + id.len())])
+                        .unwrap()
+                        .to_string()
+                    // e.g. base_path = "/webdav/fc127fba"
+                };
+                let self_host = req
+                    .headers()
+                    .get("host")
+                    .and_then(|h| h.to_str().ok())
+                    .unwrap_or(&format!("127.0.0.1:{}", self_port))
+                    .to_string();
+                let (base_path_1, self_host_1) = (base_path.clone(), self_host.clone());
+                (
+                    move |uri: &str| -> String {
+                        let uri = uri.replace(&self_host, &format!("127.0.0.1:{}", server.port));
+                        if let Some(base_url) = server.base_url {
+                            uri.replace(&base_path, base_url)
+                        } else {
+                            uri
+                        }
+                    },
+                    move |uri: &str| -> String {
+                        let uri = uri.replace(&format!("127.0.0.1:{}", server.port), &self_host_1);
+                        if let Some(base_url) = server.base_url {
+                            uri.replace(base_url, &base_path_1)
+                        } else {
+                            uri
+                        }
+                    },
+                )
+            };
+
+            // transform request url
+            *req.uri_mut() =
+                Uri::from_str(&transform_uri_fordward(&req.uri().to_string())).unwrap_or_default();
+
+            // transform request url in "Host", "Destination" and "Referer" header (used in WebDAV protocol)
+            if let Some(host) = req.headers().get("host") {
+                let host = host
+                    .to_str()
+                    .context("Failed to parse Host header")?
+                    .to_string();
+                req.headers_mut()
+                    .insert("host", transform_uri_fordward(&host).parse().unwrap());
+            }
+            if let Some(destination) = req.headers().get("destination") {
+                let destination = destination
+                    .to_str()
+                    .context("Failed to parse Destination header")?
+                    .to_string();
+                req.headers_mut().insert(
+                    "destination",
+                    transform_uri_fordward(&destination).parse().unwrap(),
+                );
+            }
+            if let Some(referer) = req.headers().get("referer") {
+                let referer = referer
+                    .to_str()
+                    .context("Failed to parse Referer header")?
+                    .to_string();
+                req.headers_mut()
+                    .insert("referer", transform_uri_fordward(&referer).parse().unwrap());
+            }
+
+            match proxy.proxy_request(req).await {
+                Ok(response) => {
+                    if server_type == &ServerType::Webdav
+                        && response
+                            .headers()
+                            .get("content-type")
+                            .and_then(|h| h.to_str().ok())
+                            .is_some_and(|h| h.contains("xml"))
+                    {
+                        tracing::info!("Transforming response body for WebDAV share with id {} to replace base url since content-type indicates it's an XML response", id);
+                        // replace base url in response body if needed (e.g. for WebDAV directory listings)
+                        let (mut parts, body) = response.into_parts();
+                        let body = axum::body::to_bytes(body, usize::MAX)
+                    .await
+                    .map_err(|e| anyhow::anyhow!("Failed to read response body for transformation (it might be too long; this might be a bug worth looking at): {}", e))?;
+                        let body = String::from_utf8_lossy(&body).to_string();
+                        let body = transform_uri_backward(&body);
+                        tracing::info!(
+                            "Original Content-Length: {:?}, new Content-Length: {}",
+                            parts.headers.get("content-length"),
+                            body.len()
+                        );
+                        parts.headers.insert("content-length", body.len().into());
+                        Ok(Body::from(body).into_response())
+                    } else {
+                        tracing::info!(
+                        "Not transforming response body for share with id {} since it's not WebDAV",
+                        id
+                    );
+                        Ok(response)
+                    }
+                }
+                Err(e) => Ok(Response::builder()
+                    .status(axum::http::StatusCode::BAD_GATEWAY)
+                    .body(Body::from(format!("Failed to proxy request: {}", e)))
+                    .unwrap()),
+            }
+        } else {
+            Ok(Response::builder()
+                .status(axum::http::StatusCode::NOT_FOUND)
+                .body(Body::from(format!("No share found for id: {}", id)))
+                .unwrap())
+        }
     }
 }
